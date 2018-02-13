153c530bc2e836d84c2d628b453beee278e421c6
Joshua Tomazin <joshua.tomazin@gmail.com>
2018-02-13 12:13:51 -0500

    Get Biwascheme.run working inside Max 7

--- src/platforms/browser/release_initializer.js
+++ src/platforms/browser/release_initializer.js
@@ -3,44 +3,78 @@
 //
 // This file is put on the end the lib/biwascheme.js.
 //
-(function(){ //local namespace
-  var dumper = null;
-  if ($("#biwascheme-debugger")[0]) {
-    dumper = new BiwaScheme.Dumper($("#biwascheme-debugger")[0]);
-  }
+/* (function(){ //local namespace
+ *   var dumper = null;
+ *   if ($("#biwascheme-debugger")[0]) {
+ *     dumper = new BiwaScheme.Dumper($("#biwascheme-debugger")[0]);
+ *   }
+ * 
+ *   // Error handler (show message to console div)
+ *   var onError = function(e, state){
+ *     BiwaScheme.Port.current_error.put_string(e.message + "\n");
+ *     if (dumper) {
+ *       dumper.dump(state);
+ *       dumper.dump_move(1);
+ *     } else if (typeof(console) !== "undefined" && console.error) {
+ *       console.error(e.message);
+ *     } else {
+ *       throw(e);
+ *     }
+ *   };
+ * 
+ *   var run = function(script) {
+ *     var intp = new BiwaScheme.Interpreter(onError);
+ *     try{
+ *       intp.evaluate(script, function(){});
+ *     }
+ *     catch(e){
+ *       onError(e);
+ *     }
+ *   };
+ * 
+ *   // Start user's program (old style)
+ *   var script = $("script[src$='biwascheme.js']").html() ||
+ *                $("script[src$='biwascheme-min.js']").html();
+ *   if (script) run(script);
+ * 
+ *   // Start user's program (new style)
+ *   $(function(){
+ *     $("script[type='text/biwascheme']").each(function(){
+ *       run($(this).html());
+ *     });
+ *   });
+ * })();*/
 
+(function(){
   // Error handler (show message to console div)
   var onError = function(e, state){
-    BiwaScheme.Port.current_error.put_string(e.message + "\n");
-    if (dumper) {
-      dumper.dump(state);
-      dumper.dump_move(1);
-    } else if (typeof(console) !== "undefined" && console.error) {
-      console.error(e.message);
-    } else {
-      throw(e);
-    }
+    //BiwaScheme.Port.current_error.put_string(e.message + "\n");
+    //if (dumper) {
+    //dumper.dump(state);
+    //dumper.dump_move(1);
+    //} else if (typeof(console) !== "undefined" && console.error) {
+    //console.error(e.message);
+    //} else {
+    post("\nERROR:");
+    post(JSON.stringify(e));
+    post("\nstate:");
+    post(JSON.stringify(state));
+    throw(e);
+    //}
   };
 
   var run = function(script) {
     var intp = new BiwaScheme.Interpreter(onError);
     try{
-      intp.evaluate(script, function(){});
+      return intp.evaluate(script, function(){});
     }
     catch(e){
       onError(e);
     }
   };
 
-  // Start user's program (old style)
-  var script = $("script[src$='biwascheme.js']").html() ||
-               $("script[src$='biwascheme-min.js']").html();
-  if (script) run(script);
-
-  // Start user's program (new style)
-  $(function(){
-    $("script[type='text/biwascheme']").each(function(){
-      run($(this).html());
-    });
-  });
-})();
+  /* post("this is run:");
+   * post(run);
+   * post(_.range)*/
+  exports.run = run
+}).call(this);
